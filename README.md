2ba0251601d1bbb48277a8a450138931911b127e
manythumbed <manythumbed@users.noreply.github.com>
2011-11-02 21:09:06 +0000

    Beginnings.

---
+++ README.md
@@ -0,0 +1,2 @@
+dibnah is old fashioned nonsense. It may do something useful eventually but for
+now it is primarily for fun.
--- types.go
+++ types.go
@@ -1,3 +1,21 @@
 package dibnah
 
+type Type struct {
+	Name string
+}
 
+type Attribute struct {
+	Type Type
+	Name string
+}
+
+type Value struct {}
+
+type Heading struct {
+	attributes map[string] Attribute
+}
+
+type Tuple struct {
+	Heading Heading
+	Values map[string] Value
+}

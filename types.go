5d7eeb0d4d3e0f6ab7a445756c0d298b41fe8c0c
manythumbed <manythumbed@users.noreply.github.com>
2011-11-03 21:25:11 +0000

    Added heading equality

--- types.go
+++ types.go
@@ -23,7 +23,32 @@ func (h Heading) Types() []Type {
 	return t
 }
 
+func (h Heading) Names() []string {
+	n := []string{}
+	for k, _ := range h.attributes {
+		n = append(n, k)
+	}
+
+	return n
+}
+
+func (h Heading) Equals(that Heading) bool {
+	if len(h.attributes) == len(that.attributes)	{
+		for k, v := range h.attributes	{
+			if that.attributes[k].Name != v.Name {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
+
 type Tuple struct {
 	Heading Heading
 	Values map[string] Value
+}
+
+func (t Tuple) Equals(that Tuple) bool {
+	return false
 }
--- types_test.go
+++ types_test.go
@@ -27,10 +27,41 @@ func (s *S) TestEmptyHeading(c *C) {
 	h := Heading{}
 	c.Check(h.Degree(), Equals, 0)
 	c.Check(len(h.Types()), Equals, 0)
+	c.Check(h.Equals(h), Equals, true)
 }
 
 func (s *S) TestHeading(c *C) {
 	h := Heading{s.attributes}
 	c.Check(h.Degree(), Equals, len(s.attributes))
 	c.Check(len(h.Types()), Equals, 3)
+	c.Check(h.Equals(h), Equals, true)
+}
+
+func (s *S) TestHeadingEquality(c *C)	{
+	h := Heading{s.attributes}
+	c.Check(h.Equals(h), Equals, true)
+
+	h1 := Heading{}
+	c.Check(h.Equals(h1), Equals, false)
+	c.Check(h1.Equals(h), Equals, false)
+
+	a1 := map[string] Type {
+		"a" : Type{"a"},
+		"b" : Type{"b"},
+	}
+	a2 := map[string] Type {
+		"a" : Type{"b"},
+		"b" : Type{"a"},
+	}
+
+	h2 := Heading{a1}
+	c.Check(h2.Equals(h2), Equals, true)
+
+	h3 := Heading{a2}
+	c.Check(h3.Equals(h3), Equals, true)
+
+	c.Check(h2.Equals(h3), Equals, false)
+	c.Check(h3.Equals(h2), Equals, false)
+
+	
 }
